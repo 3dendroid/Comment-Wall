798d8e8fd5a17c2914aac4fdc541e87484a4a9ba
denis sazonov <denis.sazonov@mytona.com>
2025-05-11 13:16:38 +0700

    checking postgre db

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import os
+
 from flask import Flask, request, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_sqlalchemy import SQLAlchemy
 
 # Create Flask application
 app = Flask(__name__)
 
 # Configure SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comments.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'DATABASE_URL',  # use database URL from railway
+    'sqlite:///comments.db'  # fallback for local development
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -26,25 +30,31 @@ ADMIN_TOKEN = os.getenv('ADMIN_TOKEN', 'secret123')
 with open("bad_words.txt", encoding="utf-8") as f:
     BAD_WORDS = set(line.strip().lower() for line in f if line.strip())
 
+
 def contains_bad_words(text: str) -> bool:
     return any(bad in text.lower() for bad in BAD_WORDS)
+
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(500), nullable=False)
 
+
 with app.app_context():
     db.create_all()
+
 
 @app.route('/')
 def index():
     comments = Comment.query.order_by(Comment.id.desc()).all()
     return render_template('index.html', comments=comments)
 
+
 @app.route('/comments', methods=['GET'])
 def get_comments():
     comments = Comment.query.order_by(Comment.id.desc()).all()
     return jsonify([{'id': c.id, 'text': c.text} for c in comments])
+
 
 @app.route('/comments', methods=['POST'])
 @limiter.limit("1 per 5 seconds")
@@ -61,6 +71,7 @@ def post_comment():
     # возвращаем только что созданный комментарий
     return jsonify({"id": comment.id, "text": comment.text}), 201
 
+
 @app.route('/comments/<int:id>', methods=['DELETE'])
 def delete_comment(id):
     token = request.headers.get('Authorization', '')
@@ -73,12 +84,14 @@ def delete_comment(id):
     db.session.commit()
     return jsonify({"message": "Deleted"}), 200
 
+
 @app.route('/admin-verify', methods=['POST'])
 def verify_admin():
     token = request.headers.get('Authorization', '')
     if token != f"Bearer {ADMIN_TOKEN}":
         return jsonify({"error": "Unauthorized"}), 401
     return jsonify({"message": "OK"}), 200
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
